c1b59ffac03e02771780234835dce7a0c0a119b1
SAITO, Tetsuya <saito.tetsuya@gmail.com>
2021-02-10 01:41:56 +0900

    Initial commit

---
+++ src/main.h
@@ -0,0 +1,9 @@
+#if defined(TARGET_M5STACK)
+#include <M5Stack.h>
+#elif defined(TARGET_M5STICK)
+#include <M5StickC.h>
+#elif defined(TARGET_M5ATOM)
+#include <M5Atom.h>
+#elif defined(TARGET_M5PAPER)
+#include <M5EPD.h>
+#endif
